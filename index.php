2448a8c965f64c0180e18d88d270273ae3b65d8d
francisco chogas <roalchogs@gmail.com>
2016-03-06 21:32:20 -0500

    primer mundo del proyecto 3

---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+ <html>
+ <head>
+ 	<meta charset="utf-8">
+ 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+ 	<title></title>
+ 	<link rel="stylesheet" href="public/css/bootstrap.css">
+ 	<link rel="stylesheet" href="estilos.css">
+ 
+ </head>
+ <body>
+ <h1>CUZCO</h1>
+ <p>El Cuzco o Cusco5 (en quechua sureño: Qusqu o Qosqo, pronunciado [ˈqo̝s.qɔ]) es una ciudad del sureste del Perú ubicada en la vertiente oriental de la cordillera de los Andes, en la cuenca del río Huatanay, afluente del Vilcanota. Es la capital del departamento del Cuzco y, además, según está declarado en la constitución peruana, es la «capital histórica» del país.
+
+La ciudad del Cuzco, según el Instituto Nacional de Estadística e Informática, es la octava ciudad más poblada del Perú, y albergaba, en el 2014, una población de 420, 137 habitantes.6
+
+Antiguamente fue la capital del Imperio inca y una de las ciudades más importantes del Virreinato del Perú, en cuya época, y en manos de los españoles, se engalanó de iglesias, palacios y plazas barrocas y neoclásicas, que es lo que hoy le convierten en el principal lugar turístico de Perú. Declarada Patrimonio de la Humanidad en 1983 por la Unesco, suele ser denominada, debido a la gran cantidad de monumentos que posee, la "Roma de América".</p>
+ <div class="container">
+ <nav class="navbar navbar-default" role="navigation">
+ 		<div class="container-fluid">
+ 			<!-- Brand and toggle get grouped for better mobile display -->
+ 			<div class="navbar-header">
+ 				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+ 					<span class="sr-only">Toggle navigation</span>
+ 					<span class="icon-bar"></span>
+ 					<span class="icon-bar"></span>
+ 					<span class="icon-bar"></span>
+ 				</button>
+ 				<a class="navbar-brand" href="#">TIENDA VIRTUAL</a>
+ 			</div>
+ 	
+ 			<!-- Collect the nav links, forms, and other content for toggling -->
+ 			<div class="collapse navbar-collapse navbar-ex1-collapse">
+ 				<ul class="nav navbar-nav">
+ 					<li class="active"><a href="#">imagenes</a></li>
+ 					<li><a href="#">Link</a></li>
+ 				</ul>
+ 				<form class="navbar-form navbar-left" role="buscar">
+ 					<div class="form-group">
+ 						<input type="text" class="form-control" placeholder="Search">
+ 					</div>
+ 					<button type="submit" class="btn btn-default">Submit</button>
+ 				</form>
+ 				<ul class="nav navbar-nav navbar-right">
+ 					<li><a href="#">Link</a></li>
+ 					<li class="dropdown">
+ 						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+ 						<ul class="dropdown-menu">
+ 							<li><a href="#">Action</a></li>
+ 							<li><a href="#">Another action</a></li>
+ 							<li><a href="#">Something else here</a></li>
+ 							<li><a href="#">Separated link</a></li>
+ 						</ul>
+ 					</li>
+ 				</ul>
+ 			</div><!-- /.navbar-collapse -->
+ 		</div>
+ 	</nav>
+ </div>
+ 
+ 
+ <div id="carousel-id" class="carousel slide" data-ride="carousel">
+ 	<ol class="carousel-indicators">
+ 		<li data-target="#carousel-id" data-slide-to="0" class=""></li>
+ 		<li data-target="#carousel-id" data-slide-to="1" class=""></li>
+ 		<li data-target="#carousel-id" data-slide-to="2" class="active"></li>
+ 	</ol>
+ 	<div class="carousel-inner">
+ 		<div class="item">
+ 			<img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" alt="First slide" src="public/img/1.jpg">
+ 			<div class="container">
+ 				<div class="carousel-caption">
+ 					<h1></h1>
+ 					<p><code></code></p>
+ 					<p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+ 				</div>
+ 			</div>
+ 		</div>
+ 		<div class="item">
+ 			<img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" alt="First slide" src="public/img/2.jpg">
+ 			<div class="container">
+ 				<div class="carousel-caption">
+ 					<h1></h1>
+ 					<p><code></code></p>
+ 					<p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+ 				</div>
+ 			</div>
+ 		</div>
+ 		<div class="item">
+ 			<img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" alt="First slide" src="public/img/4.jpg">
+ 			<div class="container">
+ 				<div class="carousel-caption">
+ 					<h1></h1>
+ 					<p><code></code></p>
+ 					<p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+ 				</div>
+ 			</div>
+ 		</div><div class="item">
+ 			<img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" alt="First slide" src="public/img/5.jpg">
+ 			<div class="container">
+ 				<div class="carousel-caption">
+ 					<h1></h1>
+ 					<p><code></code></p>
+ 					<p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+ 				</div>
+ 			</div>
+ 		</div>
+ 		<div class="item active">
+ 			<img data-src="holder.js/900x500/auto/#555:#5a5a5a/text:Third slide" alt="Third slide" src="public/img/6.jpg">
+ 			<div class="container">
+ 				<div class="carousel-caption">
+ 					<h1></h1>
+ 					<p></p>
+ 					<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+ 				</div>
+ 			</div>
+ 		</div>
+ 	</div>
+ 	<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+ 	<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+ </div>
+ <div>
+ 	<h1>VIDEOS SUREÑOS</h1>
+ </div> 
+ <div class="container">	 	  
+ 	<div class="row">
+ 		<div class="col-md-4">
+ 			<div class="mi-video">
+ 				<video autobuffer autoloop loop controls>
+ 					<source src="public/video/20.mp4">
+ 				</video>
+ 			</div>
+ 		</div>
+ 		<div class="col-md-4">
+ 			<div class="mi-video">
+ 				<video autobuffer autoloop loop controls>
+ 					<source src="public/video/20.mp4">
+ 				</video>
+ 			</div>
+ 		</div>
+ 	<div class="col-md-4">
+ 			<div class="mi-video">
+ 				<video autobuffer autoloop loop controls>
+ 					<source src="public/video/20.mp4">
+ 				</video>
+ 			</div>
+ 		</div>
+	<div class="col-md-4">
+ 			<div class="mi-video">
+ 				<video autobuffer autoloop loop controls>
+ 					<source src="public/video/20.mp4">
+ 				</video>
+ 			</div>
+ 		</div>
+ 		<div class="col-md-4">
+ 			<div class="mi-video">
+ 				<video autobuffer autoloop loop controls>
+ 					<source src="public/video/20.mp4">
+ 				</video>
+ 			</div>
+ 		</div>
+ 		<div class="col-md-4">
+ 			<div class="mi-video">
+ 				<video autobuffer autoloop loop controls>
+ 					<source src="public/video/20.mp4">
+ 				</video>
+ 			</div>
+ 		</div>
+ </div>
+ 	<<script src="public/js/jquery-1.12.1.js"></script>
+ 	<<script src="public/js/bootstrap.js"></script>
+ </body>
+ </html> 
